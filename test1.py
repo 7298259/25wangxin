0df6b721ddf70c402052c3079df6c8a77e6d1c03
7298259 <7298259@qq.com>
2017-11-07 14:29:46 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+class Student:
+   '所有学生的基类'
+   stuCount = 0
+ 
+   def __init__(self, name,stu_no,class_no,gender):
+      self.name = name      
+      self.stu_no = stu_no
+      self.class_no = class_no
+      self.gender = gender
+      Student.stuCount += 1
+
+   def study(self):
+       print "Student can study"
+
+
+      
+class PrimaryStudent(Student):
+   primaryStuCount =0
+   
+   def canRecite(self):
+        print "Primary Student can recite"
+
+   def canOral(self):
+        print"Primary Student can oral"
+
+        
+class MiddleStudent(Student):
+    middleStuCount = 0
+
+    def __init__(self, name,stu_no,class_no,gender):
+      self.name = name      
+      self.stu_no = stu_no
+      self.class_no = class_no
+      self.gender = gender
+      Student.stuCount += 1
+      MiddleStudent.middleStuCount += 1
+    
+    def canChemistry(self):
+        print "middle Student can Chemistry"
+
+    def canPyhics(self):
+        print "middle Student can Physics "
+
+    
