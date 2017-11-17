cf91c2fc9b0585a836f53f71d8e63a04ca776cef
unknown <7298259@qq.com>
2017-11-17 14:03:19 +0800

    first commit

--- 异常处理/test1.py
+++ 异常处理/test1.py
@@ -2,10 +2,10 @@
 # -*- coding: UTF-8 -*-
 
 try:
-    fh = open("testfile2.txt", "w")
+    fh = open("C:/test1/testfile.txt", "w")
     fh.write("这是一个测试文件，用于测试异常!!")
 except IOError:
     print "Error: 没有找到文件或读取文件失败"
-finally:
-    print "close the file"
+else:
+    print "内容写入文件成功"
     fh.close()
